18662aa2dc776f5d32818d2ae193b251d1f63300
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-30 20:42:20 +0100

    Add files via upload

---
+++ Tron.py
@@ -0,0 +1,13 @@
+import platform
+import os
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('touch .prox.txt')
+except:pass
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+	__import__("Troyy").keyx()
+elif 'aarch' in arc:
+	__import__("Troy").keyx()
+else:
+	exit(f' Unknow device machine {arc}')
