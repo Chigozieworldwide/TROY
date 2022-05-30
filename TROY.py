c1e75756ebc33f5bd7dcd44043c4a4405f4ea406
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-30 20:44:33 +0100

    TROY

--- TROY.py
+++ TROY.py
@@ -6,8 +6,8 @@ try:os.system('touch .prox.txt')
 except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("Troyy").keyx()
+	__import__("TROYY").keyx()
 elif 'aarch' in arc:
-	__import__("Troy").keyx()
+	__import__("TROY").keyx()
 else:
 	exit(f' Unknow device machine {arc}')
