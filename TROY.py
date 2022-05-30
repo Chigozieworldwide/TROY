bed12e7710088fa1990b59cd5889978a6bfdee25
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-30 20:43:42 +0100

    Delete TROY.py

--- TROY.py
+++
@@ -1,1 +0,0 @@
-import platformimport osos.system('termux-setup-storage')os.system('git pull')try:os.system('touch .prox.txt')except:passarc = str(platform.uname().machine)if 'arm' in arc:	__import__("Troyy").keyx()elif 'aarch' in arc:	__import__("Troy").keyx()else:	exit(f' Unknow device machine {arc}')
