7e0db3741ea74c7fae442eedba766ab948e280d7
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-30 20:55:29 +0100

    Update and rename TROY.py to Troy.py

--- Troy.py
+++ Troy.py
@@ -6,8 +6,8 @@ try:os.system('touch .prox.txt')
 except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("TROYY").keyx()
+	__import__("Troyy").keyx()
 elif 'aarch' in arc:
-	__import__("TROY").keyx()
+	__import__("Troy").keyx()
 else:
 	exit(f' Unknow device machine {arc}')
