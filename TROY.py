76b86c7d12f0e7124ea9b5d27f4b23cca944f2e5
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-06-01 19:13:01 +0100

    .

--- TROY.py
+++ TROY.py
@@ -8,6 +8,6 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("TROY").keyx()
 elif 'aarch' in arc:
-	__import__("TROY").keyx()
+	__import__("TROY64").keyx()
 else:
 	exit(f' Unknow device machine {arc}')
