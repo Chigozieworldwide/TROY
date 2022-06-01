03effd640112b2ff87aa18e3cb63d993d9237e11
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-06-01 18:53:53 +0100

    .

--- Troy.py
+++ Troy.py
@@ -10,6 +10,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("Troy").Keyx()
+		__import__("Troy")._site_view_()
 	else:
-		__import__("Troyy").Keyx()
+		__import__("Troyy")._site_view_()
